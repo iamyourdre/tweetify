a7de5a45b6c55e82634804e54407ebfda7eec348
iamyourdre <iamyourdre@gmail.com>
2025-01-05 11:55:34 +0700

    feat: add logout functionality to profile header; integrate logout action in user profile

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -9,6 +9,7 @@ import Post from '../../components/Post';
 import Repost from '../../components/Repost';
 import Loading from '../../components/Loading';
 import { useAuthContext } from '../../contexts/AuthContext';
+import useLogout from '../../hooks/useLogout';
 
 const Profile = () => {
   const {user: me} = useAuthContext();
@@ -18,6 +19,7 @@ const Profile = () => {
   const { following, followers, isFollowing, loading, followUser, unfollowUser } = useFollowUser(user?._id);
   const { getPostsByUserId, loading: postsLoading } = useGetPosts();
   const { userPosts } = usePost();
+  const { logout } = useLogout();
 
   useEffect(() => {
     if (users[0]) {
@@ -32,7 +34,7 @@ const Profile = () => {
     <div className='min-h-screen border-r border-gray-600'>
       {user && 
         <>
-          <ProfileHeader user={user} isFollowing={isFollowing} followUser={followUser} unfollowUser={unfollowUser} following={following} followers={followers} meId={me._id} />
+          <ProfileHeader user={user} isFollowing={isFollowing} followUser={followUser} unfollowUser={unfollowUser} following={following} followers={followers} meId={me._id} logout={logout} />
           <div className='px-4 py-6 lg:px-10 text-xl font-semibold border-t border-b border-gray-600'>Posts & Replies</div>
           <div className="">
             {postsLoading && 
@@ -62,7 +64,7 @@ const Profile = () => {
   );
 };
 
-const ProfileHeader = ({ user, isFollowing, followUser, unfollowUser, following, followers, meId }) => {
+const ProfileHeader = ({ user, isFollowing, followUser, unfollowUser, following, followers, meId, logout }) => {
   return (
     <div className="flex items-start gap-4 px-4 py-16 lg:px-10 bg-base-300">
       <div className="flex flex-col">
@@ -74,8 +76,6 @@ const ProfileHeader = ({ user, isFollowing, followUser, unfollowUser, following,
           </div>
         </div>
         <div className="mt-6 flex gap-4">
-          {/* <h2>Following: {following.length}</h2>
-          <h2>Followers: {followers.length}</h2> */}
           <p className='text-gray-500'><span className='font-bold text-gray-50'>{followers.length}</span> Followers</p>
           <p className='text-gray-500'><span className='font-bold text-gray-50'>{following.length}</span> Following</p>
         </div>
@@ -87,8 +87,8 @@ const ProfileHeader = ({ user, isFollowing, followUser, unfollowUser, following,
             addClass={isFollowing ? "btn-outline" : ''} 
             onClick={isFollowing ? unfollowUser : followUser} 
           />
-        ) : (''
-          // <Button text="Edit Profile" addClass="btn-outline" />
+        ) : (
+          <Button text="Logout" addClass="btn-outline btn-error" onClick={logout} />
         )}
       </div>
     </div>
